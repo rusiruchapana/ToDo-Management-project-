519422531d36383a7fb71e974240f2918ca53d32
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-05-13 11:41:30 +0530

    Started the To Do management project.

---
+++ springboot-To-Do-management-project/src/main/java/com/rusiruchapana/springboot/To/Do/management/project/SpringbootToDoManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rusiruchapana.springboot.To.Do.management.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootToDoManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootToDoManagementProjectApplication.class, args);
+	}
+
+}
---
+++ springboot-To-Do-management-project/src/test/java/com/rusiruchapana/springboot/To/Do/management/project/SpringbootToDoManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusiruchapana.springboot.To.Do.management.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootToDoManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
