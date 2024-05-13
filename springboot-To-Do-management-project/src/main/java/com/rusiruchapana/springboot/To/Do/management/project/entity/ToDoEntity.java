bdc59eea19ca0d097de8bb03cb3e454cb564fc78
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-05-13 12:51:36 +0530

    Create ToDo JPA entity.

---
+++ springboot-To-Do-management-project/src/main/java/com/rusiruchapana/springboot/To/Do/management/project/entity/ToDoEntity.java
@@ -0,0 +1,31 @@
+package com.rusiruchapana.springboot.To.Do.management.project.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Table(name = "todos")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToDoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+    
+    private Boolean completed;
+
+}
