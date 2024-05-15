641d9cf8b6b33ec8ae056875cc5e97b634efedba
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-05-15 08:27:20 +0530

    Using modelmapper third party library convert dto into jpa entity in vise versa.

--- springboot-To-Do-management-project/src/main/java/com/rusiruchapana/springboot/To/Do/management/project/SpringbootToDoManagementProjectApplication.java
+++ springboot-To-Do-management-project/src/main/java/com/rusiruchapana/springboot/To/Do/management/project/SpringbootToDoManagementProjectApplication.java
@@ -1,10 +1,17 @@
 package com.rusiruchapana.springboot.To.Do.management.project;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootToDoManagementProjectApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootToDoManagementProjectApplication.class, args);
--- springboot-To-Do-management-project/src/main/java/com/rusiruchapana/springboot/To/Do/management/project/service/impl/ToDoServiceImpl.java
+++ springboot-To-Do-management-project/src/main/java/com/rusiruchapana/springboot/To/Do/management/project/service/impl/ToDoServiceImpl.java
@@ -5,6 +5,7 @@ import com.rusiruchapana.springboot.To.Do.management.project.entity.ToDoEntity;
 import com.rusiruchapana.springboot.To.Do.management.project.repository.ToDoRepository;
 import com.rusiruchapana.springboot.To.Do.management.project.service.ToDoService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -12,24 +13,16 @@ import org.springframework.stereotype.Service;
 public class ToDoServiceImpl implements ToDoService {
 
     private ToDoRepository toDoRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public ToDoDto addToDo(ToDoDto toDoDto) {
 
-        ToDoEntity toDoEntity = new ToDoEntity();
-        toDoEntity.setTitle(toDoDto.getTitle());
-        toDoEntity.setDescription(toDoDto.getDescription());
-        toDoEntity.setCompleted(toDoDto.isCompleted());
+        ToDoEntity toDoEntity = modelMapper.map(toDoDto , ToDoEntity.class);
 
         toDoRepository.save(toDoEntity);
-
-        ToDoDto toDoDto1 = new ToDoDto();
-        toDoDto1.setId(toDoEntity.getId());
-        toDoDto1.setTitle(toDoEntity.getTitle());
-        toDoDto1.setDescription(toDoEntity.getDescription());
-        toDoDto1.setCompleted(toDoEntity.getCompleted());
-
-
+        
+        ToDoDto toDoDto1 = modelMapper.map(toDoEntity , ToDoDto.class);
 
         return toDoDto1;
     }
